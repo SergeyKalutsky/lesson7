c19ff52ea7500b62bbad3b08b19eeac5cea09bd9
sergey <skalutsky@gmail.com>
2021-05-04 11:29:06 +0300

    fist commit

---
+++ helpers.py
@@ -0,0 +1,109 @@
+import pretrained_networks
+from google_drive_downloader import GoogleDriveDownloader as gdd
+import tensorflow as tf
+import numpy as np
+import dnnlib
+import dnnlib.tflib as tflib
+import PIL.Image
+from tqdm import tqdm
+from math import ceil
+import scipy
+from IPython import display as ipythondisplay
+import io
+import os
+import base64
+from IPython.display import HTML
+
+def generate_zs_from_seeds(seeds):
+    zs = []
+    for seed_idx, seed in enumerate(seeds):
+        rnd = np.random.RandomState(seed)
+        z = rnd.randn(1, *Gs.input_shape[1:]) # [minibatch, component]
+        zs.append(z)
+    return zs
+
+
+def generate_images(zs, truncation_psi):
+    Gs_kwargs = dnnlib.EasyDict()
+    Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    Gs_kwargs.randomize_noise = False
+    if not isinstance(truncation_psi, list):
+        truncation_psi = [truncation_psi] * len(zs)
+        
+    imgs = []
+    for z_idx, z in tqdm(enumerate(zs)):
+        Gs_kwargs.truncation_psi = truncation_psi[z_idx]
+        noise_rnd = np.random.RandomState(1) # fix noise
+        tflib.set_vars({var: noise_rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
+        images = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
+        imgs.append(PIL.Image.fromarray(images[0], 'RGB'))
+    return imgs
+
+def generate_images_from_seeds(seeds, truncation_psi):
+    return generate_images(generate_zs_from_seeds(seeds), truncation_psi)
+
+
+def createImageGrid(images, scale=0.25, rows=1):
+   w,h = images[0].size
+   w = int(w*scale)
+   h = int(h*scale)
+   height = rows*h
+   cols = ceil(len(images) / rows)
+   width = cols*w
+   canvas = PIL.Image.new('RGBA', (width,height), 'white')
+   for i,img in enumerate(images):
+     img = img.resize((w,h), PIL.Image.ANTIALIAS)
+     canvas.paste(img, (w*(i % cols), h*(i // cols))) 
+   return canvas
+
+
+def interpolate(zs, steps):
+   out = []
+   for i in range(len(zs)-1):
+    for index in range(steps):
+     fraction = index/float(steps) 
+     out.append(zs[i+1]*fraction + zs[i]*(1-fraction))
+   return out
+
+
+
+def create_image_grid(images, grid_size=None):
+    assert images.ndim == 3 or images.ndim == 4
+    num, img_h, img_w, channels = images.shape
+
+    if grid_size is not None:
+        grid_w, grid_h = tuple(grid_size)
+    else:
+        grid_w = max(int(np.ceil(np.sqrt(num))), 1)
+        grid_h = max((num - 1) // grid_w + 1, 1)
+
+    grid = np.zeros([grid_h * img_h, grid_w * img_w, channels], dtype=images.dtype)
+    for idx in range(num):
+        x = (idx % grid_w) * img_w
+        y = (idx // grid_w) * img_h
+        grid[y : y + img_h, x : x + img_w] = images[idx]
+    return grid
+
+# Frame generation func for moviepy.
+def make_frame(t):
+    frame_idx = int(np.clip(np.round(t * fps), 0, num_frames - 1))
+    latents = all_latents[frame_idx]
+    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    images = Gs.run(latents, None, truncation_psi=truncation_psi,
+                          randomize_noise=False, output_transform=fmt, 
+                          minibatch_size=16)
+
+    grid = create_image_grid(images, grid_size)
+    if image_zoom > 1:
+        grid = scipy.ndimage.zoom(grid, [image_zoom, image_zoom, 1], order=0)
+    if grid.shape[2] == 1:
+        grid = grid.repeat(3, 2) # grayscale => RGB
+    return grid
+
+def show_video(vid):
+  ext = os.path.splitext(vid)[-1][1:]
+  video = io.open(vid, 'r+b').read()
+  ipythondisplay.display(HTML(data='''<video alt="test" autoplay 
+              loop controls style="height: 400px;">
+              <source src="data:video/{1}';base64,{0}" type="video/{1}" />
+              </video>'''.format(base64.b64encode(video).decode('ascii'), ext)))
